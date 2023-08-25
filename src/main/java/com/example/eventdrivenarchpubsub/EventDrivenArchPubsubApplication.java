6cd8d6e53b4dd59342dabed2f534c80dca9c92a2
Temple Okosun <okosun.temple@gmail.com>
2023-08-25 15:42:45 -0400

    initial

---
+++ src/main/java/com/example/eventdrivenarchpubsub/EventDrivenArchPubsubApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventdrivenarchpubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenArchPubsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenArchPubsubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eventdrivenarchpubsub/EventDrivenArchPubsubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventdrivenarchpubsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenArchPubsubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
